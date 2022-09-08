2feb02db96d1944c1000a3f8741f1e93f35ce8e0
Yaşar İÇLİ <yasaricli@gmail.com>
2022-09-08 16:13:30 +0300

    Added TouchableOpacity this.props
    TouchableOpacity props are not working.
    
    Sample:
    ```
        <DoubleTap style={{ padding: 5 }} />
    ```

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ export default class DoubleTap extends Component {
 
   render() {
     return (
-      <TouchableOpacity onPress={this._onTap}>
+      <TouchableOpacity onPress={this._onTap} {...this.props}>
         {this.props.children}
       </TouchableOpacity>
     );
